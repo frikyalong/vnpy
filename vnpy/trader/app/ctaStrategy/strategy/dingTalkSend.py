777c20412ebdbfa5eda971c4b07ee5644598f40f
Gary Wang <wangyangiam@Hotmail.com>
2018-11-07 17:12:27 +0800

    update dingding

--- vnpy/trader/app/ctaStrategy/strategy/dingTalkSend.py
+++ vnpy/trader/app/ctaStrategy/strategy/dingTalkSend.py
@@ -1,4 +1,4 @@
-import urllib
+import urllib, requests
 import json
 
 class dingRobot():
@@ -7,20 +7,16 @@ class dingRobot():
         self.url = "https://oapi.dingtalk.com/robot/send?access_token=637a6624701c9393e8d2056e59951f024cfdbb5042c89dcf8fb4fcd489c7c4d5"
 
     def request(self, url, method, data=None, head={}):
-        request = urllib.Request(url=url, headers=head)
-        request.get_method = lambda: method
-        httpRes = urllib.urlopen(request, data)
+        rq = requests.get(url=url, headers=head)
+        httpRes = urllib.request.urlopen(rq, data)
         content = httpRes.read()
         httpRes.close()
         return content
 
     def postStart(self, infoContent):
-        data = {}
-        data['msgtype'] = 'markdown'
-        data['markdown'] = {}
-        data['markdown']['title'] = '监控信息'
-        data['markdown']['text'] = infoContent
-        data = json.dumps(data)
-        head = {"Content-Type": "application/json"}
-        content = self.request(self.url, "POST", data, head)
-        return content
+        program = {
+            "msgtype": "text",
+            "text": {"content": infoContent},
+        }
+        headers = {'Content-Type': 'application/json'}
+        f = requests.post(self.url, data=json.dumps(program), headers=headers)
