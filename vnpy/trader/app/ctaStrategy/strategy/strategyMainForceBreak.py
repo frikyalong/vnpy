ea267f7966423e6bc7cd6391528d7e8bed3bb121
yangwang <yangwang@appannie.com>
2018-11-08 14:08:45 +0800

    fix error

--- vnpy/trader/app/ctaStrategy/strategy/strategyMainForceBreak.py
+++ vnpy/trader/app/ctaStrategy/strategy/strategyMainForceBreak.py
@@ -72,14 +72,41 @@ class MainForceBreakStrategy(CtaTemplate):
                  'trailingPercent']
 
     # 变量列表，保存了变量的名称
-    varList = ['inited',
-               'trading',
-               'pos',
-               'atrValue',
-               'atrMa',
-               'rsiValue',
-               'rsiBuy',
-               'rsiSell']
+    varList = {
+        'isBigChange': False,
+        'isContinuousRise': True,
+        'm5HighValue': 0,
+        'm5HighOpenInterest': 0,
+        'm5HighVolume': 0,
+        'm5HighChange': 0,
+        'm5LowValue': 0,
+        'm5LowOpenInterest': 0,
+        'm5LowVolume': 0,
+        'm5LowChange': 0,
+        'm5PreVolume': 0,
+        'm5CurVolume': 0,
+        'm5PreOpenInterest': 0,
+        'm5CurOpenInterest': 0,
+        'm5PreChangeArray': np.zeros(5),
+        'm3HighValue': 0,
+        'm3HighOpenInterest': 0,
+        'm3HighVolume': 0,
+        'm3HighChange': 0,
+        'm3ChangeArray': np.zeros(5),
+        'm3LowValue': 0,
+        'm3LowOpenInterest': 0,
+        'm3LowVolume': 0,
+        'm3LowChange': 0,
+        'm3PreVolume': 0,
+        'm3CurVolume': 0,
+        'm3PreOpenInterest': 0,
+        'm3CurOpenInterest': 0,
+        'm3PreChangeArray': np.zeros(5),
+        'fiveMinK': 5,
+        'fiveMinKCount': 0,
+        'threeMinK': 9,
+        'threeMinKCount': 0,
+    }
 
     # ----------------------------------------------------------------------
     def __init__(self, ctaEngine, setting):
@@ -103,55 +130,128 @@ class MainForceBreakStrategy(CtaTemplate):
 
         m5RBLineSetting = copy.copy(m5LineSettingBase)
         m5RBLineSetting['shortSymbol'] = 'RB'
+        m5RBLineSetting['name'] = 'M5RB'
+        m3RBLineSetting = copy.copy(m5RBLineSetting)
+        m3RBLineSetting['name'] = 'M3RB'
+        m3RBLineSetting['barTimeInterval'] = 3
 
         m5RULineSetting = copy.copy(m5LineSettingBase)
         m5RULineSetting['shortSymbol'] = 'RU'
+        m5RULineSetting['name'] = 'M5RU'
+        m3RULineSetting = copy.copy(m5RULineSetting)
+        m3RULineSetting['name'] = 'M3RU'
+        m3RULineSetting['barTimeInterval'] = 3
 
         m5MLineSetting = copy.copy(m5LineSettingBase)
         m5MLineSetting['shortSymbol'] = 'M'
+        m5MLineSetting['name'] = 'M5M'
+        m3MLineSetting = copy.copy(m5MLineSetting)
+        m3MLineSetting['name'] = 'M3M'
+        m3MLineSetting['barTimeInterval'] = 3
 
         m5ILineSetting = copy.copy(m5LineSettingBase)
         m5ILineSetting['shortSymbol'] = 'I'
+        m5ILineSetting['name'] = 'M5I'
+        m3ILineSetting = copy.copy(m5ILineSetting)
+        m3ILineSetting['name'] = 'M3I'
+        m3ILineSetting['barTimeInterval'] = 3
 
         m5CULineSetting = copy.copy(m5LineSettingBase)
         m5CULineSetting['shortSymbol'] = 'CU'
+        m5CULineSetting['name'] = 'M5CU'
+        m3CULineSetting = copy.copy(m5CULineSetting)
+        m3CULineSetting['name'] = 'M3CU'
+        m3CULineSetting['barTimeInterval'] = 3
 
         m5NILineSetting = copy.copy(m5LineSettingBase)
         m5NILineSetting['shortSymbol'] = 'NI'
+        m5NILineSetting['name'] = 'M5NI'
+        m3NILineSetting = copy.copy(m5NILineSetting)
+        m3NILineSetting['name'] = 'M3NI'
+        m3NILineSetting['barTimeInterval'] = 3
 
         m5HCLineSetting = copy.copy(m5LineSettingBase)
         m5HCLineSetting['shortSymbol'] = 'HC'
+        m5HCLineSetting['name'] = 'M5HC'
+        m3HCLineSetting = copy.copy(m5HCLineSetting)
+        m3HCLineSetting['name'] = 'M3HC'
+        m3HCLineSetting['barTimeInterval'] = 3
 
         m5YLineSetting = copy.copy(m5LineSettingBase)
         m5YLineSetting['shortSymbol'] = 'Y'
+        m5YLineSetting['name'] = 'M5Y'
+        m3YLineSetting = copy.copy(m5YLineSetting)
+        m3YLineSetting['name'] = 'M3Y'
+        m3YLineSetting['barTimeInterval'] = 3
+
+        m5JMLineSetting = copy.copy(m5LineSettingBase)
+        m5JMLineSetting['shortSymbol'] = 'JM'
+        m5JMLineSetting['name'] = 'M5JM'
+        m3JMLineSetting = copy.copy(m5JMLineSetting)
+        m3JMLineSetting['name'] = 'M3JM'
+        m3JMLineSetting['barTimeInterval'] = 3
 
         m5CFLineSetting = copy.copy(m5LineSettingBase)
-        m5CFLineSetting['shortSymbol'] = 'JM'
-
-        m5RBLineSetting = copy.copy(m5LineSettingBase)
-        m5RBLineSetting['shortSymbol'] = 'CF'
+        m5CFLineSetting['shortSymbol'] = 'CF'
+        m5CFLineSetting['name'] = 'M5CF'
+        m3CFLineSetting = copy.copy(m5CFLineSetting)
+        m3CFLineSetting['name'] = 'M3CF'
+        m3CFLineSetting['barTimeInterval'] = 3
 
         m5ZNLineSetting = copy.copy(m5LineSettingBase)
         m5ZNLineSetting['shortSymbol'] = 'ZN'
+        m5ZNLineSetting['name'] = 'M5ZN'
+        m3ZNLineSetting = copy.copy(m5ZNLineSetting)
+        m3ZNLineSetting['name'] = 'M3ZN'
+        m3ZNLineSetting['barTimeInterval'] = 3
 
         m5SRLineSetting = copy.copy(m5LineSettingBase)
         m5SRLineSetting['shortSymbol'] = 'SR'
+        m5SRLineSetting['name'] = 'M5SR'
+        m3SRLineSetting = copy.copy(m5SRLineSetting)
+        m3SRLineSetting['name'] = 'M3SR'
+        m3SRLineSetting['barTimeInterval'] = 3
 
-        # MARKET = {
-        #     'market': {name: 'RB', m5Setting: m5RBLineSetting},
-        #     'market': {name: 'RU', m5Setting: m5RULineSetting},
-        #     'market': {name: 'M', m5Setting: m5MLineSetting},
-        #     'market': {name: 'I', m5Setting: m5ILineSetting},
-        #     'market': {name: 'CU', m5Setting: m5CULineSetting},
-        #     'market': {name: 'NI', m5Setting: m5NILineSetting},
-        #     'market': {name: 'HC', m5Setting: m5HCLineSetting},
-        #     'market': {name: 'Y', m5Setting: m5YLineSetting},
-        #     'market': {name: 'JM', m5Setting: m5CFLineSetting},
-        #     'market': {name: 'CF', m5Setting: m5RBLineSetting},
-        #     'market': {name: 'ZN', m5Setting: m5ZNLineSetting},
-        #     'market': {name: 'SR', m5Setting: m5SRLineSetting},
-        # }
+        MARKET = {
+            'RB': {'name': 'RB', 'm5Setting': m5RBLineSetting, 'm5Setting': m5RBLineSetting, 'varList': copy.copy(self.varList)},
+            'RU': {'name': 'RU', 'm5Setting': m5RULineSetting, 'm5Setting': m5RULineSetting, 'varList': copy.copy(self.varList)},
+            'M': {'name': 'M', 'm5Setting': m5MLineSetting, 'm5Setting': m5MLineSetting, 'varList': copy.copy(self.varList)},
+            'I': {'name': 'I', 'm5Setting': m5ILineSetting, 'm5Setting': m5ILineSetting, 'varList': copy.copy(self.varList)},
+            'CU': {'name': 'CU', 'm5Setting': m5CULineSetting, 'm5Setting': m5CULineSetting, 'varList': copy.copy(self.varList)},
+            'NI': {'name': 'NI', 'm5Setting': m5NILineSetting, 'm5Setting': m5NILineSetting, 'varList': copy.copy(self.varList)},
+            'HC': {'name': 'HC', 'm5Setting': m5HCLineSetting, 'm5Setting': m5HCLineSetting, 'varList': copy.copy(self.varList)},
+            'Y': {'name': 'Y', 'm5Setting': m5YLineSetting, 'm5Setting': m5YLineSetting, 'varList': copy.copy(self.varList)},
+            'JM': {'name': 'JM', 'm5Setting': m5CFLineSetting, 'm5Setting': m5CFLineSetting, 'varList': copy.copy(self.varList)},
+            'CF': {'name': 'CF', 'm5Setting': m5RBLineSetting, 'm5Setting': m5RBLineSetting, 'varList': copy.copy(self.varList)},
+            'ZN': {'name': 'ZN', 'm5Setting': m5ZNLineSetting, 'm5Setting': m5ZNLineSetting, 'varList': copy.copy(self.varList)},
+            'SR': {'name': 'SR', 'm5Setting': m5SRLineSetting, 'm5Setting': m5SRLineSetting, 'varList': copy.copy(self.varList)},
+        }
 
+        self.lineM5RB = bar_class(self, self.onBarM5, MARKET['RB'].m5RBLineSetting)
+        self.lineM5RU = bar_class(self, self.onBarM5, MARKET['RU'].m5RBLineSetting)
+        self.lineM5M = bar_class(self, self.onBarM5, MARKET['M'].m5RBLineSetting)
+        self.lineM5I = bar_class(self, self.onBarM5, MARKET['I'].m5RBLineSetting)
+        self.lineM5CU = bar_class(self, self.onBarM5, MARKET['CU'].m5RBLineSetting)
+        self.lineM5NI = bar_class(self, self.onBarM5, MARKET['NI'].m5RBLineSetting)
+        self.lineM5HC = bar_class(self, self.onBarM5, MARKET['HC'].m5RBLineSetting)
+        self.lineM5Y = bar_class(self, self.onBarM5, MARKET['Y'].m5RBLineSetting)
+        self.lineM5JM = bar_class(self, self.onBarM5, MARKET['JM'].m5RBLineSetting)
+        self.lineM5CF = bar_class(self, self.onBarM5, MARKET['CF'].m5RBLineSetting)
+        self.lineM5ZN = bar_class(self, self.onBarM5, MARKET['ZN'].m5RBLineSetting)
+        self.lineM5SR = bar_class(self, self.onBarM5, MARKET['SR'].m5RBLineSetting)
+
+        self.lineM3RB = bar_class(self, self.onBarM3, MARKET['RB'].m3LineSetting)
+        self.lineM3RU = bar_class(self, self.onBarM3, MARKET['RU'].m3LineSetting)
+        self.lineM3M = bar_class(self, self.onBarM3, MARKET['M'].m3LineSetting)
+        self.lineM3I = bar_class(self, self.onBarM3, MARKET['I'].m3LineSetting)
+        self.lineM3CU = bar_class(self, self.onBarM3, MARKET['CU'].m3LineSetting)
+        self.lineM3NI = bar_class(self, self.onBarM3, MARKET['NI'].m3LineSetting)
+        self.lineM3HC = bar_class(self, self.onBarM3, MARKET['HC'].m3LineSetting)
+        self.lineM3Y = bar_class(self, self.onBarM3, MARKET['Y'].m3LineSetting)
+        self.lineM3JM = bar_class(self, self.onBarM3, MARKET['JM'].m3LineSetting)
+        self.lineM3CF = bar_class(self, self.onBarM3, MARKET['CF'].m3LineSetting)
+        self.lineM3ZN = bar_class(self, self.onBarM3, MARKET['ZN'].m3LineSetting)
+        self.lineM3SR = bar_class(self, self.onBarM3, MARKET['SR'].m3LineSetting)
 
         # 创建5minsK线
         m5LineSetting = {}
@@ -202,36 +302,43 @@ class MainForceBreakStrategy(CtaTemplate):
             if tick.vtSymbol == 'rb1901':
                 self.lineM5.onTick(copy.copy(tick))
                 self.lineM3.onTick(copy.copy(tick))
+                self.lineM5RB.onTick(copy.copy(tick))
+                self.lineM3RB.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'ru1901':
+                self.lineM5RU.onTick(copy.copy(tick))
+                self.lineM3RU.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'm1901':
+                self.lineM5M.onTick(copy.copy(tick))
+                self.lineM3M.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'i1901':
+                self.lineM5I.onTick(copy.copy(tick))
+                self.lineM3I.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'cu1901':
+                self.lineM5CU.onTick(copy.copy(tick))
+                self.lineM3CU.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'ni1901':
+                self.lineM5NI.onTick(copy.copy(tick))
+                self.lineM3NI.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'hc1901':
+                self.lineM5HC.onTick(copy.copy(tick))
+                self.lineM3HC.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'y1901':
+                self.lineM5Y.onTick(copy.copy(tick))
+                self.lineM3Y.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'jm1901':
+                self.lineM5JM.onTick(copy.copy(tick))
+                self.lineM3JM.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'cf1901':
+                self.lineM5CF.onTick(copy.copy(tick))
+                self.lineM3CF.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'zn1901':
+                self.lineM5ZN.onTick(copy.copy(tick))
+                self.lineM3ZN.onTick(copy.copy(tick))
+            elif tick.vtSymbol == 'sr1901':
+                self.lineM5SR.onTick(copy.copy(tick))
+                self.lineM3SR.onTick(copy.copy(tick))
 
-        tickMinute = tick.datetime.minute
 
-        if tickMinute != self.barMinute:
-            if self.bar:
-                self.onBar(self.bar)
-
-            bar = CtaBarData()
-            bar.vtSymbol = tick.vtSymbol
-            bar.symbol = tick.symbol
-            bar.exchange = tick.exchange
-
-            bar.open = tick.lastPrice
-            bar.high = tick.lastPrice
-            bar.low = tick.lastPrice
-            bar.close = tick.lastPrice
-            bar.openInterest = tick.openInterest
-
-            bar.date = tick.date
-            bar.time = tick.time
-            bar.datetime = tick.datetime  # K线的时间设为第一个Tick的时间
-
-            self.bar = bar  # 这种写法为了减少一层访问，加快速度
-            self.barMinute = tickMinute  # 更新当前的分钟
-        else:  # 否则继续累加新的K线
-            bar = self.bar  # 写法同样为了加快速度
-
-            bar.high = max(bar.high, tick.lastPrice)
-            bar.low = min(bar.low, tick.lastPrice)
-            bar.close = tick.lastPrice
 
     # ----------------------------------------------------------------------
     def onBar(self, bar):
@@ -239,6 +346,8 @@ class MainForceBreakStrategy(CtaTemplate):
         self.putEvent()
 
     def onBarM5(self, m5Bar):
+        print('*' * 20 + 'onTick start' + '*' * 20)
+        print('\n'.join(['%s:%s' % item for item in m5Bar.__dict__.items()]))
         self.writeCtaLog(u'onBarM5')
         self.writeCtaLog(self.lineM5.displayLastBar())
 
